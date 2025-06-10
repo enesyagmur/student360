87ad21b5b0b36692eed79e7a528062bec9421c6f
erain <enesyagmur99@gmail.com>
2025-06-10 20:22:07 +0300

    yönetici işlemleri için firbase admin kurulumu

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,7 +1,7 @@
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { deleteUser, signInWithEmailAndPassword } from "firebase/auth";
 import { db, auth } from "../../lib/firebase";
 
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
 
 export const userLoginService = async (email, password, role) => {
   try {
@@ -30,5 +30,55 @@ export const userLoginService = async (email, password, role) => {
   } catch (err) {
     console.error("SERVICE | giriş yaparken sorun ", err);
     return err;
+  }
+};
+
+export const deleteManagerService = async (
+  currentUserId,
+  managerIdForDelete
+) => {
+  if (!currentUserId) {
+    throw new Error("Bu işlemi yapmak için giriş yapmanız gerekiyor");
+  }
+  try {
+    const userDocRef = doc(db, "users");
+    const userSnapDoc = await getDoc(userDocRef);
+    if (!userSnapDoc.exists()) {
+      throw new Error("SERVICE | Kullanıcılar verisi bulunamadı");
+    }
+
+    const users = userSnapDoc.data();
+
+    const currentManager = users.find(
+      (manager) => manager.id === currentUserId
+    );
+
+    if (!currentManager || currentManager.position !== "principal") {
+      throw new Error(
+        "SERVICE | Bu işlem için yetkiniz yok ya da bilgileriniz bulunamadı."
+      );
+    }
+
+    const managerForDelete = users.find(
+      (manager) => manager.id === managerIdForDelete
+    );
+
+    if (!managerForDelete) {
+      throw new Error("SERVICE | Silinecek yönetici bilgileri bulunamadı.");
+    }
+
+    const newUsers = users.filter(
+      (manager) => manager.id !== managerIdForDelete
+    );
+
+    await updateDoc(userDocRef, {
+      newUsers,
+    });
+
+    deleteUser(managerIdForDelete);
+
+    return managerIdForDelete;
+  } catch (err) {
+    console.error("SERVICE | Yönetici silme işleminde sorun ", err);
   }
 };
